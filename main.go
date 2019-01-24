1827079725162fa82c54b6d8163d49ec0c73193d
Stephano Z <mail@szfs.eu>
2019-01-24 13:13:31 +0100

    Add main package and test.

---
+++ main.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+)
+
+var port = 8080
+
+func indexHandler(w http.ResponseWriter, r *http.Request) {
+	io.WriteString(w, "Hey! This is just a demo page. :-)")
+}
+
+func main() {
+	http.HandleFunc("/", indexHandler)
+
+	fmt.Println("Now listening on port", fmt.Sprintf("%d", port))
+	log.Fatal(http.ListenAndServe(":"+fmt.Sprintf("%d", port), nil))
+}
---
+++ main_test.go
@@ -0,0 +1,50 @@
+package main
+
+import (
+	"io/ioutil"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+)
+
+var writer *httptest.ResponseRecorder
+
+func setup() {
+	writer = httptest.NewRecorder()
+}
+
+func TestIndexGet200(t *testing.T) {
+	setup()
+	expected := http.StatusOK
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal("Creating 'GET /' request failed!")
+	}
+
+	indexHandler(writer, req)
+	resp := writer.Result()
+
+	if resp.StatusCode != expected {
+		t.Fatal("Server error: Returned code:", resp.StatusCode, "instead of:", expected)
+	}
+}
+
+func TestIndexBody(t *testing.T) {
+	setup()
+	expected := "Hey! This is just a demo page. :-)"
+
+	req, err := http.NewRequest("GET", "/", nil)
+	if err != nil {
+		t.Fatal("Creating 'GET /' request failed!")
+	}
+
+	indexHandler(writer, req)
+
+	resp := writer.Result()
+	body, _ := ioutil.ReadAll(resp.Body)
+
+	if string(body) != expected {
+		t.Fatal("Error: Returned body:", string(body), "instead of:", expected)
+	}
+}
