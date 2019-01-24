c416a7982038e0c2bcce118d1e365aa8a780f2dd
Stephano Z <mail@szfs.eu>
2019-01-24 19:56:22 +0100

    Change greeting.

--- main_test.go
+++ main_test.go
@@ -32,7 +32,7 @@ func TestIndexGet200(t *testing.T) {
 
 func TestIndexBody(t *testing.T) {
 	setup()
-	expected := "Hey! This is just a demo page. :-)"
+	expected := "Yo! This is just a demo page. :-)"
 
 	req, err := http.NewRequest("GET", "/", nil)
 	if err != nil {
